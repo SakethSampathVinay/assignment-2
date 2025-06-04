69c4cb4e3334467fe7f57c617b6d1d9c47d3cb6f
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-06-04 15:03:29 +0530

    assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'assignment-2';
+
+  name: string = "";
+  subject: string = "";
+  testScore1: number = 0;
+  testScore2: number = 0;
 }
